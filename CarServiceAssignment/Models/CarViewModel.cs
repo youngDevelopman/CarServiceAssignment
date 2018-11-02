54919412578b90b887fb1e0fd87b235cc7357a77
young_developman <nazarmazurik99@gmail.com>
2018-11-02 17:40:42 +0200

    Label improvements

--- CarServiceAssignment/Models/CarViewModel.cs
+++ CarServiceAssignment/Models/CarViewModel.cs
@@ -13,26 +13,26 @@ namespace CarServiceAssignment.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage ="Please, enter the correct brand name")]
+        [Display(Name = "Brand")]
         public string Brand { get; set; }
 
         [Required(ErrorMessage = "Please, enter the correct model name")]
+        [Display(Name = "Model")]
         public string Model { get; set; }
 
         [Required(ErrorMessage = "Please, enter the correct type")]
+        [Display(Name = "Type")]
         public string Type { get; set; }
 
         [Required(ErrorMessage = "Please, enter the correct price")]
+        [Display(Name = "Price")]
         public decimal? Price { get; set; }
 
         [Required(ErrorMessage ="Please,enter the correct year")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [Display(Name = "Year")]
         public DateTime? Year { get; set; }
 
         public virtual ICollection<OwnerViewModel> CarOwners { get; set; }
-
-        public CarViewModel()
-        {
-            CarOwners = new List<OwnerViewModel>();
-        }
     }
 }
--- CarServiceAssignment/Models/OwnerViewModel.cs
+++ CarServiceAssignment/Models/OwnerViewModel.cs
@@ -10,17 +10,22 @@ namespace CarServiceAssignment.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Please, enter the correct first name")]
+        [Display(Name = "Fisrt name")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Please, enter the correct last name")]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Please, enter the correct birthdate")]
+        [Display(Name = "Date of birth")]
         public DateTime? Birthdate { get; set; }
 
         [Required(ErrorMessage = "Please, enter the correct driving experience")]
+        [Display(Name = "Driving experience")]
         public string DrivingExperience { get; set; }
 
+        [Display(Name = "Which car he/she has")]
         public virtual ICollection<CarViewModel> CarOwners { get; set; }
     }
 }
