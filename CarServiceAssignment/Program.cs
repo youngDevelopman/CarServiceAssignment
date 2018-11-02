596d9eba16903ccdd05cbe85e82848988ec4ebce
young_developman <nazarmazurik99@gmail.com>
2018-11-02 21:36:50 +0200

    Self-contained deployment configuration

--- CarServiceAssignment/Program.cs
+++ CarServiceAssignment/Program.cs
@@ -14,7 +14,26 @@ namespace CarServiceAssignment
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            WebHost.CreateDefaultBuilder()
+   .ConfigureAppConfiguration((hostingContext, config) =>
+   {
+        // Get the environment from our hostContext.
+        var env = hostingContext.HostingEnvironment;
+       config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        // Build an initial configuration.
+        IConfiguration Configuration = config.Build();
+
+        // Set the environment name.
+        env.EnvironmentName = Configuration.GetSection("ActiveEnvironment").Value;
+
+        // Load the configuration file for our specific environment.
+        config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: false, reloadOnChange: true)
+       .AddEnvironmentVariables();
+   })
+   .UseStartup<Startup>()
+   .Build()
+   .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
